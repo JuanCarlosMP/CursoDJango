51dc929586aad1356de5fc1ede3f78aeeed7d5cd
Juan Carlos <jcmorales23@hotmail.com>
2025-01-22 14:26:51 -0600

    actualizacion_2

--- coffe_project/settings.py
+++ coffe_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_tailwind',
     'products'
 ]
 
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'coffe_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +125,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuracion para archivos que se cargan
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'tailwind'
+CRISPY_TEMPLATE_PACK = 'tailwind'
--- products/views.py
+++ products/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from products.forms import ProductForm
 from products.models import Product
+from django.views import generic
+from django.urls import reverse_lazy
 
 # Create your views here.
-class ListProductsView(TemplateView):
+# class ListProductsView(TemplateView):
+#     template_name = 'products/list_products.html'
+
+#     def get_context_data(self, **kwargs):
+#         products = Product.objects.all()
+
+#         context = super().get_context_data(**kwargs)
+#         context['list_products'] = products
+
+#         return context
+
+class ListProductsView(generic.ListView):
     template_name = 'products/list_products.html'
+    model = Product
+    context_object_name = 'list_products'
 
-    def get_context_data(self, **kwargs):
-        products = Product.object.all()
+class ProductFormView(generic.FormView):
+    template_name = 'products/add_product.html'
+    form_class = ProductForm
+    success_url = reverse_lazy('list_product')
 
-        context = super().get_context_data(**kwargs)
-        context['list_products'] = products
-
-        return context
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
